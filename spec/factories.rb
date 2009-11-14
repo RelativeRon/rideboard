e722709b8df07600654374f90129a7bc7c72a92c
tonyschneider <tsnydermtg@gmail.com>
2009-11-14 12:38:28 -0500

    switched previously written tests to factory_girl, need to refactor

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,3 +4,11 @@ Factory.define :user do |u|
   u.password 'password'
   u.password_confirmation 'password'
 end
+
+Factory.define :driver_post do |dp|
+  user = Factory.create(:user)
+  dp.poster_id user.id
+  dp.title "Able to drive to CWD Web Dev Day"
+  dp.origin "Athens"
+  dp.message "I would like to split gas."
+end
--- spec/models/driver_post_spec.rb
+++ spec/models/driver_post_spec.rb
@@ -1,30 +1,22 @@
 require 'spec_helper'
 
 describe DriverPost do
-  
-  user = Factory.create(:user)
-
-  before(:each) do
-    @valid_attributes = {
-      :poster_id => user.id,
-      :title => "My Cool Title",
-      :origin => "Columbus",
-      :message => "Suck it."
-    }
-  end
 
   it "should create a new instance given valid attributes" do
-    DriverPost.create!(@valid_attributes)
+    Factory.build(:driver_post)
   end
 
   it "should have a driver" do
-    dp = DriverPost.new(@valid_attributes)
-    dp.driver.should_not be_nil
+    Factory.build(:driver_post).driver.should_not be_nil
   end
 
   it "should have a its poster as its driver" do
-    dp = DriverPost.create(@valid_attributes)
+    dp = Factory.build(:driver_post)
     dp.driver.should == dp.poster
+  end
+  
+  it "should have a type of poster" do
+    pending
   end
 
   after(:all) do
