5f19901479ff8e511e29d01c0dee5e97ff9fd469
Jerry Nummi <jerry@theedgecase.com>
2010-01-07 09:54:59 -0500

    Gem configuration updates.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,5 +27,9 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
+config.gem "thoughtbot-factory_girl",
+           :lib    => "factory_girl",
+           :source => "http://gems.github.com"
+
 # For Clearance
 HOST = "localhost"
