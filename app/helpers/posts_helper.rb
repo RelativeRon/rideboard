28a27678478dac55e8a2b3f3ede7694651975830
Jerry Nummi <jerry@theedgecase.com>
2010-01-10 22:15:00 -0500

    New layout for forms.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,4 @@
 module PostsHelper
-  def posts_header_tag
-    text = (params[:action] == 'mine' ? "My Posts" : "Current Posts")
-    "<h1>#{text}</h1>"
-  end
-
   def posts_view_toggle_link_tag
     if params[:action] == 'mine'
       link_to "View all posts", posts_path
